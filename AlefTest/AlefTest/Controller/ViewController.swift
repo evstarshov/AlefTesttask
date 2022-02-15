168e2c445fed84c235d656287cbb2abd8854a9e9
EugeneStarshov <yeenzo@yandex.ru>
2022-02-15 18:50:01 +0300

    clear table button

--- AlefTest/AlefTest/Controller/ViewController.swift
+++ AlefTest/AlefTest/Controller/ViewController.swift
@@ -9,14 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private var maxChildren = 5
     
-    var childrenArray = [Child]()
+    private var childrenArray = [Child]()
     
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var ageTextField: UITextField!
     @IBOutlet weak var childrenTableView: UITableView!
     @IBOutlet weak var addChildButton: UIButton!
+    @IBOutlet weak var clearTableButton: UIButton!
     
     private struct Constants {
         static let reuseIdentifier = "Cell"
@@ -30,12 +32,28 @@ class ViewController: UIViewController {
         childrenTableView.reloadData()
     }
 
-    @IBAction func addChild() {
-        let name = nameTextField.text
-        let age = ageTextField.text
-        //guard (name != nil) && age != "" else { return }
-        childrenArray.append(Child(name: name!, age: age!))
+    @IBAction func addChildButtonTapped() {
+        if maxChildren > 0 {
+        appendChild()
         childrenTableView.reloadData()
+        } else {
+            return
+        }
+    }
+    
+    @IBAction func clearTableButtonTapped() {
+        print("clearing table")
+        childrenArray.removeAll()
+        childrenTableView.reloadData()
+    }
+    
+    private func appendChild() {
+        guard nameTextField.text != "" else { return }
+        let name = nameTextField.text
+        guard ageTextField.text != "" else { return }
+        let age = ageTextField.text
+        childrenArray.append(Child(name: name!, age: age!))
+        maxChildren -= 1
     }
 
 }
